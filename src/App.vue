84bde6008538c45bc913315560cc0936ebc979ef
Aleksei Bezdetko <ggalliosha@gmail.com>
2020-06-08 12:14:38 +0300

    Initialize project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quotes: [
+        'Just a quote',
+      ],
+      maxQuotes: 10,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
