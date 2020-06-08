392fe066209c12322c7f1f94372182c04b13ab7b
Aleksei Bezdetko <ggalliosha@gmail.com>
2020-06-08 12:35:18 +0300

    Create components

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-row class="row"></b-row>
+</template>
+
+<script>
+export default {
+  props: ['quotes'],
+};
+</script>
+
+<style>
+.row {
+  font-family: 'Bad Script', cursive;
+  font-size: 2rem;
+  color: lightgray;
+}
+</style>
