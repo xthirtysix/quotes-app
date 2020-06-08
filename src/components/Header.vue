a2d6a81c422e1a7b7828fdaa9b6d9e8259e5c33d
Aleksei Bezdetko <ggalliosha@gmail.com>
2020-06-08 14:00:00 +0300

    Add progressbar

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-row>
+    <b-col sm="12">
+      <h3>Quotes added</h3>
+       <b-progress
+       :max="maxQuotes">
+        <b-progress-bar :value="quoteCount"> {{quoteCount}} / {{maxQuotes}} </b-progress-bar>
+       </b-progress>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  props: [
+    'quoteCount',
+    'maxQuotes',
+  ],
+};
+</script>
